7ffd2c223970965b00b71bdb891fbdbe67e93b4e
Jhay Cudia <jrcudia1@gmail.com>
2023-05-23 18:24:15 -0400

    added apiUrl after line 23

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function imageLoaded() {
         // loader.hidden = true;
         console.log('ready =', ready);
         count = 30;
+        const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
     }
 }
 // Helper Function to Set Attributes on DOM Elements
